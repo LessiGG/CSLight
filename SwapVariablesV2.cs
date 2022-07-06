14d4d0fa2d33cca4004046350a00da6c13bc1929
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-06 22:02:51 +0300

    Add files via upload

---
+++ SwapVariablesV2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSLight
+{
+    class SwapVariablesV2
+    {
+        static void Main(string[] args)
+        {
+            string name = "Пушкин";
+            string surname = "Александр";
+
+            Console.WriteLine($"До перестановки: имя {name}, фамилия {surname}");
+            
+            (name, surname) = (surname, name);
+            
+            Console.WriteLine($"После перестановки: имя {name}, фамилия {surname}");
+        }
+    }
+}
