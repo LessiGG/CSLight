82e4188b7a1a2863143a76a4989f35828e35dec6
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-06 21:32:42 +0300

    Add files via upload

---
+++ TenVariables.cs
@@ -0,0 +1,19 @@
+﻿namespace CSLight
+{
+    class TenVariables
+    {
+        static void Main(string[] args)
+        {
+            int wallet;
+            uint tasksAmount;
+            float numberPi;
+            double centsCount;
+            string userName;
+            char botPrefix;
+            short carPrice;
+            ushort infectedCount;
+            ulong earthPopulation;
+            bool isChestLocked;
+        }
+    }
+}
