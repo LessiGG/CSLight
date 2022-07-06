0a139f5384f4187310f3036416c590d341db96c6
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-06 22:25:38 +0300

    Add files via upload

---
+++ HospitalQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSLight
+{
+    class HospitalQueue
+    {
+        static void Main(string[] args)
+        {
+            int peopleInQueue;
+            int waitTime = 10;
+
+            Console.Write("Введите количество людей в очереди: ");
+            peopleInQueue = Convert.ToInt32(Console.ReadLine());
+
+            int hours = peopleInQueue * waitTime / 60;
+            int minutes = peopleInQueue * waitTime % 60;
+            
+            Console.WriteLine($"Вы должны отстоять в очереди {hours} часов и {minutes} минут");
+        }
+    }
+}
