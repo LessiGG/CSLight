13db63992bc9da61fb15d37d171ca3434ae37f5e
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-06 21:55:49 +0300

    Add files via upload

---
+++ ImagesInRows.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSLight
+{
+    class ImagesInRows
+    {
+        static void Main(string[] args)
+        {
+            int imagesPerRow = 3;
+            int imagesCount = 52;
+
+            int completedRows = imagesCount / imagesPerRow;
+            int imagesLeft = imagesCount % imagesPerRow;
+            
+            Console.WriteLine($"Заполнено рядов {completedRows}, осталось картинок {imagesLeft}");
+        }
+    }
+}
