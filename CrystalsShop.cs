ccc9222365133110717f3e9a54954c62e639f8c4
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-06 22:15:44 +0300

    Add files via upload

---
+++ CrystalsShop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSLight
+{
+    class CrystalsShop
+    {
+        static void Main(string[] args)
+        {
+            int coinsCount;
+            int crystalsCount;
+            int crystalPrice = 10;
+            
+            Console.Write("Введите сколько у вас денег: ");
+            coinsCount = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write($"Введите сколько кристаллов вы хотите приобрести (стоимость одного кристалла {crystalPrice}): ");
+            crystalsCount = Convert.ToInt32(Console.ReadLine());
+
+            coinsCount -= crystalPrice * crystalsCount;
+
+            Console.WriteLine($"У вас {crystalsCount} кристаллов и {coinsCount} монет.");
+        }
+    }
+}
