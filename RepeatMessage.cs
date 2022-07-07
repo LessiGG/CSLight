9383d2e3b1034eb634314119351a1bbc52fa6a39
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-07 16:37:48 +0300

    Add files via upload

---
+++ RepeatMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSLight
+{
+    class RepeatMessage
+    {
+        static void Main(string[] args)
+        {
+            string userMessage;
+            int repeatCount;
+
+            Console.Write("Введите сообщение которое будет повторяться: ");
+            userMessage = Console.ReadLine();
+
+            Console.Write("Введите сколько раз сообщение должно повториться: ");
+            repeatCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repeatCount; i++)
+            {
+                Console.WriteLine(userMessage);
+            }
+        }
+    }
+}
