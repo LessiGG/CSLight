50f1c9a0de8560d3b4ef467871b28e3a0f5df51d
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-07 18:15:10 +0300

    Update Multiplicity.cs

--- Multiplicity.cs
+++ Multiplicity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace CSLight
 {
@@ -7,16 +7,21 @@ namespace CSLight
         static void Main(string[] args)
         {
             Random random = new Random();
-            int number = random.Next(0, 100);
+            int minValue = 0;
+            int maxValue = 100;
+            int number = random.Next(minValue, maxValue);
+            
+            int multiplicityBy3 = 3;
+            int multiplicityBy5 = 5;
             int numbersSum = 0;
 
             for (int i = number; i > 0; i--)
             {
-                if (i % 3 == 0 || i % 5 == 0)
+                if (i % multiplicityBy3 == 0 || i % multiplicityBy5 == 0)
                     numbersSum += i;
             }
 
-            Console.WriteLine($"Сумма всех чисел кратных 3 и 5 в диапазоне до {number} включительно: {numbersSum}");
+            Console.WriteLine($"Сумма всех чисел кратных {multiplicityBy3} и {multiplicityBy5} в диапазоне до {number} включительно: {numbersSum}");
         }
     }
 }
