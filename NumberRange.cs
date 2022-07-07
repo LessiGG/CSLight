fc6278b6034f8166b0582d6f6c897f4ed4fce271
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-07 17:03:24 +0300

    Add files via upload

---
+++ NumberRange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSLight
+{
+    class NumberRange
+    {
+        static void Main(string[] args)
+        {
+            int startValue = 5;
+            int endValue = 96;
+            int step = 7;
+            
+            for (int i = startValue; i <= endValue; i+= step)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
