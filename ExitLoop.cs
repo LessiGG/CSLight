7e57fc3abb49599f708121b85dc352a78100ac1e
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-07 16:57:56 +0300

    Add files via upload

---
+++ ExitLoop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSLight
+{
+    class ExitLoop
+    {
+        static void Main(string[] args)
+        {
+            string exitWord = "exit";
+            string userInput;
+            bool exitConditionMet = false;
+            
+            while (!exitConditionMet)
+            {
+                Console.Write("Если хотите выйти из программы введите exit: ");
+                userInput = Console.ReadLine();
+                
+                if (userInput == exitWord)
+                    exitConditionMet = true;
+            }
+        }
+    }
+}
