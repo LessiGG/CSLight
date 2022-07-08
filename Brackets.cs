b26c2f56bba023db352b281519ea7e249c68dfa4
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-08 19:49:33 +0300

    Add files via upload

---
+++ Brackets.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CSLight
+{
+    class Brackets
+    {
+        static void Main(string[] args)
+        {
+            string brackets = ")((()()))()(";
+            int count = 0;
+            int maxCount = 0;
+
+            foreach (var bracket in brackets)
+            {
+                if (bracket == '(')
+                {
+                    count++;
+                    
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                }
+
+                else if (bracket == ')')
+                {
+                    count--;
+                }
+                
+                if (count < 0)
+                    break;
+            }
+            
+            if (count != 0)
+                Console.WriteLine("Последовательность некоректная.");
+            else
+            {
+                Console.WriteLine(maxCount);
+            }
+        }
+    }
+}
