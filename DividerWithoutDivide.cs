255f6e50e3a2d4401320252ee06fd914f7676468
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-08 18:05:39 +0300

    Add files via upload

---
+++ DividerWithoutDivide.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSLight
+{
+    class DividerWithoutDivide
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int minRandomValue = 1;
+            int maxRandomValue = 28;
+            int N = random.Next(minRandomValue, maxRandomValue);
+            int dividersCount = 0;
+
+            int bottomBorder = 100;
+            int topBorder = 1000;
+
+            for (int i = bottomBorder; i < topBorder; i++)
+            {
+                int divider = i;
+                while (divider > 0)
+                {
+                    divider -= N;
+                }
+
+                if (divider == 0)
+                {
+                    dividersCount++;
+                }
+            }
+
+            Console.WriteLine(dividersCount);
+        }
+    }
+}
