87190e34754a6777f1827cef88f7c8bf40be6dbf
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-08 17:25:13 +0300

    Add files via upload

---
+++ SymbolsRectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSLight
+{
+    class SymbolsRectangle
+    {
+        static void Main(string[] args)
+        {
+            char rectangleSymbol;
+            string userName;
+            int cornerSymbols = 2;
+
+            Console.Write("Введите ваше имя: ");
+            userName = Console.ReadLine();
+
+            Console.Write("Введите символ: ");
+            rectangleSymbol = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < userName.Length + cornerSymbols; i++)
+            {
+                Console.Write(rectangleSymbol);
+            }
+
+            Console.WriteLine($"\n{rectangleSymbol}{userName}{rectangleSymbol}");
+            
+            for (int i = 0; i < userName.Length + 2; i++)
+            {
+                Console.Write(rectangleSymbol);
+            }
+        }
+    }
+}
