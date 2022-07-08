6abaa5d54f05d49ea49433c6edb4d8f5865f8bb7
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-08 18:44:10 +0300

    Add files via upload

---
+++ NumberInDegree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSLight
+{
+    class NumberInDegree
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minRandomValue = 0;
+            int maxRandomValue = 100;
+            int number = random.Next(minRandomValue, maxRandomValue);
+            
+            int numberToDegree = 2;
+            int degreeCount = 0;
+            int twoInDegreeOfNumber = 0;
+
+            for (int i = numberToDegree; i <= number * numberToDegree; i*= numberToDegree)
+            {
+                degreeCount++;
+                twoInDegreeOfNumber = i;
+            }
+            
+            Console.WriteLine(number);
+            Console.WriteLine(degreeCount);
+            Console.WriteLine(twoInDegreeOfNumber);
+        }
+    }
+}
