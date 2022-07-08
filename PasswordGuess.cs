2ec05dbde4ec8c5578953e8b42b50c9c370a81a6
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-08 17:30:10 +0300

    Add files via upload

---
+++ PasswordGuess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSLight
+{
+    class PasswordGuess
+    {
+        static void Main(string[] args)
+        {
+            string password = "123456";
+            string userInput;
+            int tryCount = 3;
+
+            while (tryCount-- > 0)
+            {
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Тайное сообщение");
+                    break;
+                }
+
+                Console.WriteLine($"Неверный пароль, осталось {tryCount} попыток.");
+            }
+        }
+    }
+}
