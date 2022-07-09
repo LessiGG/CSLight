b87376eaa83fd4144983ff51ee89d49fc8bb57e0
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-09 20:57:19 +0300

    Add files via upload

---
+++ ArrayTask2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CSLight
+{
+    class ArrayTask2
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            int[,] matrixA = new int[10, 10];
+            int maxMatrixValue = int.MinValue;
+            int minRandomNumber = 1;
+            int maxRandomNumber = 10;
+
+            for (int i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixA.GetLength(1); j++)
+                {
+                    matrixA[i, j] = random.Next(minRandomNumber, maxRandomNumber);
+                    
+                    if (maxMatrixValue < matrixA[i, j])
+                    {
+                        maxMatrixValue = matrixA[i, j];
+                    }
+
+                    Console.Write(matrixA[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("------------------------------");
+            
+            for (int i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixA.GetLength(1); j++)
+                {
+                    if (matrixA[i, j] == maxMatrixValue)
+                    {
+                        matrixA[i, j] = 0;
+                    }
+                    
+                    Console.Write(matrixA[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(maxMatrixValue);
+        }
+    }
+}
