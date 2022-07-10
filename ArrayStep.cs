8976e3d06ac4e74ce384ce2fa5de1e2107a10420
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-10 11:59:35 +0300

    Add files via upload

---
+++ ArrayStep.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSLight
+{
+    class ArrayStep
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 5, 12};
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                (array[i], array[i + 1]) = (array[i + 1], array[i]);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
