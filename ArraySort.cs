9a5123ac62c1ef8edd5f0ba3660276049da851aa
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-10 11:44:57 +0300

    Add files via upload

---
+++ ArraySort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSLight
+{
+    class ArraySort
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random random = new Random();
+            
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 10);
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine("\nОтсортированный массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        (array[i], array[j]) = (array[j], array[i]);
+                    }
+                }
+                
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
