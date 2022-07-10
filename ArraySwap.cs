abb51b8201271ae0298238cd1a685e09ecaa8c3b
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-10 03:23:47 +0300

    Add files via upload

---
+++ ArraySwap.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CSLight
+{
+    class ArraySwap
+    {
+        static void Main(string[] args)
+        {
+            int lengthArray = 0;
+            int[] array = new int[lengthArray];
+            int[] tempArray = new int[lengthArray];
+            int arraySum = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"Введите число, exit или sum: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput == "sum")
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        arraySum += array[i];
+                    }
+
+                    Console.WriteLine($"Сумма массива - {arraySum}");
+                    break;
+                }
+
+                if (userInput == "exit")
+                {
+                    break;
+                }
+
+                int arrayItem = Convert.ToInt32(userInput);
+                lengthArray += 1;
+
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    tempArray[i] = array[i];
+                }
+
+                array = new int[lengthArray];
+
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    array[i] = tempArray[i];
+                }
+
+                array[lengthArray - 1] = arrayItem;
+                tempArray = array;
+            }
+        }
+    }
+}
