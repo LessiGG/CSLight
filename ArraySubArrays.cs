8c015d19f1f7668f5e5b36ec3d1ebf8780a30bcf
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-10 10:48:03 +0300

    Add files via upload

---
+++ ArraySubArrays.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSLight
+{
+    class ArraySubArrays
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int count = 1;
+            int highestCount = 1;
+            
+            //int[] array = new int[30];
+            int[] array = new int[] {5, 5, 9, 9, 9, 5, 5};
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                //array[i] = random.Next(0, 10);
+
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == array[i + 1])
+                {
+                    count++;
+
+                    if (count < highestCount)
+                    {
+                        highestCount = count;
+                    }
+                }
+            }
+        }
+    }
+}
