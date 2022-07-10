2139b5a6b11424c333ff05c127eee0dd82c59121
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-10 12:30:17 +0300

    Update ArrayStep.cs

--- ArrayStep.cs
+++ ArrayStep.cs
@@ -6,13 +6,16 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            int[] array = {1, 2, 3, 4, 5, 12};
+            int[] array = {1, 2, 3, 4, 5, 12, 2, 2, 4, 5, 6, 7, 8, 88, 3, 2, 1};
             
             int steps = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < array.Length - steps; i++)
+            for (int i = 0; i < steps; i++)
             {
-                (array[i], array[i + steps]) = (array[i + steps], array[i]);
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                }
             }
 
             for (int i = 0; i < array.Length; i++)
