9a16a5a028364b09244107abe39fb5e1aa5869d9
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-10 11:50:31 +0300

    Add files via upload

---
+++ StringSplit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSLight
+{
+    class StringSplit
+    {
+        static void Main(string[] args)
+        {
+            string message = "Дана строка с текстом, используя метод строки String.Split() получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
+
+            string[] messageWords = message.Split(' ');
+
+            for (int i = 0; i < messageWords.Length; i++)
+            {
+                Console.WriteLine(messageWords[i] + " ");
+            }
+        }
+    }
+}
