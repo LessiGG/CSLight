f5789abee6cdd39134b9d21a4aa0b0cc5a95a336
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-12 21:10:42 +0300

    Add files via upload

---
+++ TryParseNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSLight
+{
+    public class TryParseNumber
+    {
+        static void Main(string[] args)
+        {
+            int result = TryGetNumber();
+            Console.WriteLine(result);
+        }
+
+        static int TryGetNumber()
+        {
+            bool isRunning = true;
+            
+            while (isRunning)
+            {
+                Console.Write("Введите число: ");
+                string userInput = Console.ReadLine();
+                bool gotNumber = int.TryParse(userInput, out int result);
+
+                if (gotNumber == false)
+                {
+                    Console.WriteLine("Пожалуйста, введите число.");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
