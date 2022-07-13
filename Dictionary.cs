036298cb94867aca2e750c0ca612796060b10c21
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-13 20:31:46 +0300

    Add files via upload

---
+++ Dictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSLight
+{
+    class Dictionary
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> translations = new Dictionary<string, string>();
+            
+            translations.Add("Яблоко", "Apple");
+
+            Console.Write("Введите слово чтобы получить его перевод: ");
+            string userInput = Console.ReadLine();
+
+            foreach (var translation in translations)
+            {
+                if (userInput != translation.Key)
+                {
+                    Console.WriteLine("На это слово нет перевода.");
+                }
+                else
+                {
+                    Console.WriteLine(translation.Value);
+                }
+            }
+        }
+    }
+}
