5d1d3262522bfbed081d9c9c16cbe8e1d1bcf2f6
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-16 17:33:15 +0300

    Update PurchaseQueue.cs

--- PurchaseQueue.cs
+++ PurchaseQueue.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace CSLight
@@ -8,46 +8,44 @@ namespace CSLight
         static void Main(string[] args)
         {
             int moneyCount = 0;
-            
-            Queue<int> clientsPurchases;
-            
-            clientsPurchases = FillPurchasesQueue();
+            Queue<int> clientsPurchases = FillClientsQueue();
 
             while (clientsPurchases.Count > 0)
             {
                 Purchase(clientsPurchases, ref moneyCount);
             }
 
-            Console.WriteLine("Все клиенты ушли с покупками!");
-            Console.ReadKey();
+            Console.WriteLine("Все клиенты обслужены.");
         }
 
-        static Queue<int> FillPurchasesQueue()
+        static Queue<int> FillClientsQueue()
         {
             Random random = new Random();
+            
             int minPurchasePrice = 100;
             int maxPurchasePrice = 1000;
+
             int minQueueLength = 10;
             int maxQueueLength = 20;
             
             int randomQueueLength = random.Next(minQueueLength, maxQueueLength);
-            Queue<int> tempClientsPurchases = new Queue<int>(randomQueueLength);
+            Queue<int> tempQueue = new Queue<int>(randomQueueLength);
 
             for (int i = 0; i < randomQueueLength; i++)
             {
-                tempClientsPurchases.Enqueue(random.Next(minPurchasePrice, maxPurchasePrice));
+                tempQueue.Enqueue(random.Next(minPurchasePrice, maxPurchasePrice));
             }
 
-            return tempClientsPurchases;
+            return tempQueue;
         }
 
-        static void Purchase(Queue<int> clientsCash, ref int moneyCount)
+        static void Purchase(Queue<int> clientsPurchases, ref int moneyCount)
         {
-            Console.WriteLine($"В очереди - {clientsCash.Count}  клиент(ов)");
-            Console.WriteLine($"Сумма в кассе:  {moneyCount}");
-            Console.WriteLine($"Нажмите любую клавишу чтобы совершить покупку на сумму: {clientsCash.Peek()}");
+            Console.WriteLine($"В очереди находятся {clientsPurchases.Count} клиентов.");
+            Console.WriteLine($"В кассе {moneyCount} денег.");
+            Console.WriteLine($"Нажмите любую клавишу чтобы совершить покупку на сумму {clientsPurchases.Peek()}");
             Console.ReadKey();
-            moneyCount += clientsCash.Dequeue();
+            moneyCount += clientsPurchases.Dequeue();
             Console.Clear();
         }
     }
