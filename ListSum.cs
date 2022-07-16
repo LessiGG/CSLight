b3d8fa6745fc4b03976ab163a821a71599ee283d
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-07-16 17:47:00 +0300

    Add files via upload

---
+++ ListSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSLight
+{
+    class ListSum
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            bool isExit = false;
+
+            string sumCommand = "sum";
+            string exitCommand = "exit";
+
+            while (isExit == false)
+            {
+                Console.WriteLine($"Введите число, {exitCommand} или {sumCommand}: ");
+                string userInput = Console.ReadLine();
+                bool gotNumber = int.TryParse(userInput, out int result);
+
+                if (gotNumber == true)
+                {
+                    numbers.Add(Convert.ToInt32(userInput));
+                }
+                else if (userInput == sumCommand)
+                {
+                    int numbersSum = 0;
+                    
+                    foreach (var number in numbers)
+                    {
+                        numbersSum += number;
+                    }
+
+                    Console.WriteLine($"Сумма введенных чисел - {numbersSum}");
+                }
+                else if (userInput == exitCommand)
+                {
+                    isExit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Было введено не число.");
+                }
+            }
+        }
+    }
+}
