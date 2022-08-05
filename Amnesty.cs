46bdf48bd73eac1c1f2fd001a97e673b6603b630
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-08-05 20:34:35 +0300

    Add files via upload

---
+++ Amnesty.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            City city = new City();
+            
+            city.ShowCitizens();
+            city.PerformAmnesty();
+            city.RemoveGuiltyCitizens();
+            city.ShowCitizens();
+        }
+    }
+
+    class Citizen
+    {
+        public string Name { get; private set; }
+        public bool Guilty { get; private set; }
+
+        public Citizen(string name)
+        {
+            Name = name;
+            Guilty = false;
+        }
+
+        public void BecomeGuilty()
+        {
+            Guilty = true;
+        }
+    }
+
+    class City
+    {
+        private readonly Random _random = new Random();
+        private List<Citizen> _citizens = new List<Citizen>();
+
+        public City()
+        {
+            _citizens.Add(new Citizen("Олег"));
+            _citizens.Add(new Citizen("Андрей"));
+            _citizens.Add(new Citizen("Вячеслав"));
+            _citizens.Add(new Citizen("Артемий"));
+            _citizens.Add(new Citizen("Максим"));
+        }
+
+        public void ShowCitizens()
+        {
+            foreach (var citizen in _citizens)
+            {
+                Console.WriteLine(citizen.Name);
+            }
+
+            Console.WriteLine("------------------");
+        }
+
+        public void RemoveGuiltyCitizens()
+        {
+            var citizens = (from Citizen citizen in _citizens
+                                      where citizen.Guilty == false
+                                      select citizen).ToList();
+            _citizens = citizens;
+        }
+
+        public void PerformAmnesty()
+        {
+            foreach (var citizen in _citizens.Where(human => _random.Next(2) == 0))
+            {
+                citizen.BecomeGuilty();
+            }
+        }
+    }
+}
